2d7ef9781b040d56048f634f1254c8531d60b0be
duconguaidia-spec <duconguaidia@gmail.com>
2026-04-29 09:35:18 -0500

    Se agrega la conexion DeleteNoticia para eliminar los datos de una noticia por su ID

--- controllers/Noticias_controller.go
+++ controllers/Noticias_controller.go
@@ -81,3 +81,12 @@ func UpdateNoticia(w http.ResponseWriter, r *http.Request) {
 	respondJSON(w, 200, map[string]string{"message": "Noticia actualizada"})
 }
 
+func DeleteNoticia(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	_, err := config.DB.Exec("DELETE FROM noticias WHERE id=$1", id)
+	if err != nil {
+		respondJSON(w, 500, map[string]string{"error": err.Error()})
+		return
+	}
+	respondJSON(w, 200, map[string]string{"message": "Noticia eliminada"})
+}
