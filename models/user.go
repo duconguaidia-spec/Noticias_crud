45e1eb3b07fefd16e51116255c9d0f347c44c260
duconguaidia-spec <duconguaidia@gmail.com>
2026-04-29 09:14:02 -0500

    Se agrega el modelo Noticia con sus respectivos campos

---
+++ models/user.go
@@ -0,0 +1,18 @@
+package models
+import "time"
+type Noticia struct {
+    ID int `json:"id"`
+    Titulo string `json:"titulo"`
+    Tipo string `json:"tipo"`
+    Cuerpo string `json:"cuerpo"`
+    UrlVideo string `json:"url_video"`
+    ImagenDestacada string `json:"imagen_destacada"`
+    Fuente string `json:"fuente"`
+    FechaNoticia string `json:"fecha_noticia"`       
+    IDUsuario int `json:"id_usuario"`
+    AccesoLimitado bool `json:"acceso_limitado"`
+    Estado string `json:"estado"`
+    Activo bool `json:"activo"`
+    FechaCreacion time.Time `json:"fecha_creacion"`     
+    FechaModificacion time.Time `json:"fecha_modificacion"`  
+}
