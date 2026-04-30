3f5e18eed740e706a01fb94b093ac425553c1736
duconguaidia-spec <duconguaidia@gmail.com>
2026-04-30 08:03:47 -0500

    Se agrega la configuracion de CORS para permitir solicitudes externas a la API

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"API_GO_CRUD/config"
+	"API_GO_CRUD/routes"
+	"github.com/gorilla/mux"
+)
+
+func enableCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if r.Method == "OPTIONS" {
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
